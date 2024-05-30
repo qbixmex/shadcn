637320af725bd9e99838a83d0470ccf1ecab0792
Daniel González Briseño <qbixmex@gmail.com>
2024-05-30 02:16:29 -0700

    alert-component

---
+++ src/components/ui/alert.tsx
@@ -0,0 +1,63 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const alertVariants = cva(
+  "relative w-full rounded-lg border px-4 py-3 text-sm [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground [&>svg~*]:pl-7",
+  {
+    variants: {
+      variant: {
+        default: "bg-background text-foreground",
+        primary: "border-blue-600 text-blue-600 dark:bg-blue-600 [&>svg]:text-blue-600",
+        warning: "border-amber-500 text-amber-600 dark:bg-amber-500 [&>svg]:text-amber-600",
+        info: "border-cyan-600 text-cyan-600 dark:bg-cyan-600 [&>svg]:text-cyan-600",
+        success: "border-green-500 text-green-800 dark:bg-green-500 [&>svg]:text-green-800",
+        destructive:
+          "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+const Alert = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div
+    ref={ref}
+    role="alert"
+    className={cn(alertVariants({ variant }), className)}
+    {...props}
+  />
+))
+Alert.displayName = "Alert"
+
+const AlertTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+AlertTitle.displayName = "AlertTitle"
+
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
+export { Alert, AlertTitle, AlertDescription }
--- src/modules/alert/alert.component.tsx
+++ src/modules/alert/alert.component.tsx
@@ -1,7 +1,69 @@
+import {
+  EarthIcon,
+  TriangleAlertIcon,
+  CheckCircleIcon,
+  InfoIcon,
+  CircleUserIcon,
+  XCircleIcon,
+  
+} from "lucide-react";
+ 
+import {
+  Alert as AlertUI,
+  AlertDescription,
+  AlertTitle,
+} from "@/components/ui/alert"
+
 export const Alert = () => {
   return (
-    <h1 className="font-bold text-5xl text-zinc-600">
-      Alert
-    </h1>
+    <div className="grid gap-3">
+      <AlertUI>
+        <EarthIcon className="h-4 w-4" />
+        <AlertTitle>Alert 1</AlertTitle>
+        <AlertDescription>
+          This is the default alert
+        </AlertDescription>
+      </AlertUI>
+
+      <AlertUI variant="primary">
+        <CircleUserIcon className="h-4 w-4" />
+        <AlertTitle>Logged In</AlertTitle>
+        <AlertDescription>
+          Welcome back Cornelius !
+        </AlertDescription>
+      </AlertUI>
+
+      <AlertUI variant="success">
+        <CheckCircleIcon className="h-4 w-4" />
+        <AlertTitle>Success</AlertTitle>
+        <AlertDescription>
+          The article was saved successfully !
+        </AlertDescription>
+      </AlertUI>
+
+      <AlertUI variant="warning">
+        <TriangleAlertIcon className="h-4 w-4" />
+        <AlertTitle>Warning</AlertTitle>
+        <AlertDescription>
+          Careful this action is irreversible !
+        </AlertDescription>
+      </AlertUI>
+
+      <AlertUI variant="info">
+        <InfoIcon className="h-4 w-4" />
+        <AlertTitle>Info</AlertTitle>
+        <AlertDescription>
+          This is just an information.
+        </AlertDescription>
+      </AlertUI>
+
+      <AlertUI variant="destructive">
+        <XCircleIcon className="h-4 w-4" />
+        <AlertTitle>Deleted</AlertTitle>
+        <AlertDescription>
+          This resource has been deleted !
+        </AlertDescription>
+      </AlertUI>
+    </div>
   );
 };
