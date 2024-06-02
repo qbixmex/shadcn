6905d997f6102e7955bdf388a755489b21ea1d3e
Daniel González Briseño <qbixmex@gmail.com>
2024-06-02 03:13:22 -0700

    table-row-actions

--- src/modules/table/columns.tsx
+++ src/modules/table/columns.tsx
@@ -4,6 +4,18 @@ import { Badge } from "@/components/ui/badge";
 import { Payment } from "@/data";
 import { ColumnDef } from "@tanstack/react-table";
 
+import { Button } from "@/components/ui/button";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
+
 export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: "clientName",
@@ -49,6 +61,42 @@ export const columns: ColumnDef<Payment>[] = [
           {formatted}
         </div>
       );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <DotsHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(payment.id);
+                toast.info(
+                  "Payment ID copied to clipboard", {
+                    duration: 2000,
+                    position: "top-right",
+                  }
+                );
+              }}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
     },
   },
 ];
