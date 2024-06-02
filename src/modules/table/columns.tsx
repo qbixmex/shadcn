9857eefe61ac78a1e4732bf2d0ec75aafa55b9ff
Daniel González Briseño <qbixmex@gmail.com>
2024-06-02 02:57:45 -0700

    table-cell-format

--- src/modules/table/columns.tsx
+++ src/modules/table/columns.tsx
@@ -1,23 +1,54 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import { Payment } from "@/data";
 import { ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<Payment>[] = [
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: "clientName",
+    header: "Client Name",
   },
   {
     accessorKey: "email",
     header: "Email",
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const status = row.getValue("status") as string;
+      const variant = {
+        success: "success",
+        failed: "destructive",
+        pending: "warning",
+        processing: "primary",
+      }[status] ?? ("default") as any;
+
+      return (
+        <Badge variant={variant} capitalize>
+          {status}
+        </Badge>
+      );
+    },
   },
   {
-    accessorKey: "clientName",
-    header: "Client Name",
+    accessorKey: "amount",
+    header: () => <p className="text-center">Amount</p>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      }).format(amount)
+ 
+      return (
+        <div className={`text-center font-medium text-${(amount < 10) ? "red" : (amount > 50) ? "green" : "blue"}-700`}>
+          {formatted}
+        </div>
+      );
+    },
   },
 ];
