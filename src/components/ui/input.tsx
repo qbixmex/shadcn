3161b07305a4484a157f29f73975f7e995344d29
Daniel González Briseño <qbixmex@gmail.com>
2024-06-03 18:02:08 -0700

    basic-form

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,20 +3,25 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    icon?: React.ReactNode;
+  }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, icon, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          type={type}
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {icon && <div className="absolute right-3 top-1/2 transform -translate-y-1/2">{icon}</div>}
+      </div>
     )
   }
 )
--- src/modules/form/form.component.tsx
+++ src/modules/form/form.component.tsx
@@ -1,15 +1,142 @@
 "use client";
 
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Title } from "../components";
+import { Button } from "@/components/ui/button";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(10),
+  email: z.string().email(),
+  password: z.string().min(6).max(20),
+  passwordConfirmation: z.string().min(6).max(20),
+}).refine(data => data.password === data.passwordConfirmation, {
+  message: "Password Confirmation does't match !",
+  path: ["passwordConfirmation"],
+});
 
 export const FormComponent = () => {
-  return (
-    <section>
-      <Title className="text-center">Card</Title>
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirmation: ""
+    },
+  });
 
-      <section className="w-[80%] mx-auto grid grid-cols-2 gap-8">
-        <p className="text-lg text-bold text-blue-500">Your form must appear here ...</p>
+  // 2. Define a submit handler.
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.table(values);
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
+
+  return (
+    <>
+      <Title className="text-center">Form</Title>
+
+      <section className="w-[80%] mx-auto">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-3">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        autoComplete="off"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        autoComplete="off"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-3">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        icon={
+                          showPassword
+                            ? <EyeIcon className="text-gray-300 cursor-pointer" onClick={() => setShowPassword(false)} />
+                            : <EyeOffIcon className="text-gray-300 cursor-pointer" onClick={() => setShowPassword(true)} />
+                        }
+                        autoComplete="off"
+                        type={showPassword ? "text" : "password"}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="passwordConfirmation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password Confirmation</FormLabel>
+                    <FormControl>
+                      <Input
+                        icon={
+                          showPasswordConfirmation
+                            ? <EyeIcon className="text-gray-300 cursor-pointer" onClick={() => setShowPasswordConfirmation(false)} />
+                            : <EyeOffIcon className="text-gray-300 cursor-pointer" onClick={() => setShowPasswordConfirmation(true)} />
+                        }
+                        autoComplete="off"
+                        type={showPasswordConfirmation ? "text" : "password"}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="text-right md:text-right">
+              <Button type="submit" variant="secondary" className="w-full md:w-fit">Submit</Button>
+            </div>
+          </form>
+        </Form>
       </section>
-    </section>
+    </>
   );
 };
